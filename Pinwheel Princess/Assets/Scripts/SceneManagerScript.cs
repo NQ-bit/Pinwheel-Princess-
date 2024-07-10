ffcbe7b322ca33bc4c38d0ab94328b4a303ba06d
NQ-bit <jquanteh96@gmail.com>
2024-07-10 16:32:31 -0400

    First Commit

---
+++ Pinwheel Princess/Assets/Scripts/SceneManagerScript.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+using System.Collections.Generic;
+
+
+
+public class SceneManagerScript : MonoBehaviour
+{
+    public void LoadScene(string CutScene)
+    {
+        SceneManager.LoadScene(CutScene);
+    }
+}
